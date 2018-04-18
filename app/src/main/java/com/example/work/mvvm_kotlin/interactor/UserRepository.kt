70da55a3de49cbbe2f00fd792b5d79467b8df36b
jasontuan <doananhtuan22111996@gmail.com>
2018-04-18 16:36:43 +0700

    hfhf

--- app/src/main/java/com/example/work/mvvm_kotlin/interactor/UserRepository.kt
+++ app/src/main/java/com/example/work/mvvm_kotlin/interactor/UserRepository.kt
@@ -29,9 +29,27 @@ class UserRepository {
 //    }
 
     fun getDataUserSearch(text: String): MutableLiveData<List<User>?> {
-        val mutableList: MutableList<User> = mutableListOf()
-        mutableList.add(User(10, "New"))
-        userLiveData.value = mutableList
-        return userLiveData
+        if (text.equals("")) {
+            databaseReference.child("Users").addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError?) {
+                }
+
+                override fun onDataChange(p0: DataSnapshot?) {
+                    val mutableList: MutableList<User> = mutableListOf()
+                    if (p0 != null) {
+                        for (item: DataSnapshot in p0.children) {
+                            mutableList.add(item.getValue(User::class.java)!!)
+                        }
+                        userLiveData.value = mutableList
+                    }
+                }
+            })
+            return userLiveData
+        } else {
+            val mutableList: MutableList<User> = mutableListOf()
+            mutableList.add(User(10, "New"))
+            userLiveData.value = mutableList
+            return userLiveData
+        }
     }
 }
